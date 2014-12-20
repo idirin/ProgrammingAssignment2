148849b4cf0afd6fdf3a34dbcb790804f2dcced9
Ricardo Martinez Idirin <idirin@acm.org>
2014-12-20 13:14:50 +0100

    First version Code base on vector/mean example

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-x
+
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinversa <- function(inversa) m <<- inversa
+  getinversa <- function() m
+  list(set = set, get = get,
+       setinversa = setinversa,
+       getinversa = getinversa)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
+  m <- x$getinversa()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinversa(m)
+  m
+  
         ## Return a matrix that is the inverse of 'x'
 }
